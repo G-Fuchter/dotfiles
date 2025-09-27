a94b728a2203bf6b7ff37da62fec233504418b66
Guillermo Fuchter <guillermo@fuchter.dev>
2025-09-27 18:23:47 +0200

    feat: Replace i3 with hyprland

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,1 +1,19 @@
 require("config.lazy")
+-- Set options
+vim.opt.termguicolors = true  -- Enable true color support
+vim.opt.number = true          -- Show line numbers
+vim.opt.relativenumber = true  -- Show relative line numbers
+vim.opt.autoindent = true      -- Auto-indent new lines
+vim.opt.tabstop = 4          -- Use 4 spaces for tabs
+vim.opt.shiftwidth = 4       -- Use 4 spaces for indenting with >> and <<
+vim.opt.expandtab = true       -- Convert tabs to spaces
+vim.opt.smartindent = true     -- Smart indenting
+vim.opt.backup = false         -- Disable creating backup files
+vim.opt.swapfile = false       -- Disable creating swap files
+vim.opt.incsearch = true       -- Incremental search
+vim.opt.hlsearch = true        -- Highlight search results
+vim.opt.hidden = true          -- Allow switching buffers without saving
+vim.opt.mouse = 'a'          -- Enable mouse support in all modes
+vim.opt.undofile = true        -- Enable persistent undo
+vim.opt.timeoutlen = 500
+vim.opt.updatetime = 200
